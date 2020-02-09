6878b2aa848d3e12d4ed22ee2ee3c227f54951ba
cmcWebCode40 <infinity_michael@yahoo.com>
2020-02-09 19:40:08 +0100

    updated the appshell and the service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -131,7 +131,7 @@ function checkValidServiceWorker(swUrl, config) {
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
-      registration.unregister();
+      registration.register();
     });
   }
 }
