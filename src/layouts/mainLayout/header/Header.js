bba299302d163264bbce0453e3677e82b9437019
unknown <infinity_michael@yahoo.com>
2020-10-29 09:46:10 +0100

    fix:eslint-warning

--- src/layouts/mainLayout/header/Header.js
+++ src/layouts/mainLayout/header/Header.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Button = styled.button.attrs({
-  className: 'btn btn-primary'
-})`color:${(props) => props.theme.colors.primary}`;
 
 const Header = () => {
   return (
