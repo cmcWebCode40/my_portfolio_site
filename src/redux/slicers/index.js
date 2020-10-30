401d25527094ea64986f052405314964ef20bd46
unknown <infinity_michael@yahoo.com>
2020-10-30 18:43:29 +0100

    chore: icons >> state mgt set up >> services

--- src/redux/slicers/index.js
+++ src/redux/slicers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import exampleSlice from './example';
+import createFair from './createfair';
 
 const rootReducer = combineReducers({
   exampleSlice,
+  createFair
 });
 
 export default rootReducer;
