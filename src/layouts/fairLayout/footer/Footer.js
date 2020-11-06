41e73bce3d35efc3a55061af7a1247b80e43cbb9
Divine Philip <dpkreativ@gmail.com>
2020-11-06 17:32:42 +0100

    set up route and header

---
+++ src/layouts/fairLayout/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <StyledDiv>
+      <footer>&copy; virtual fair 2020 Footer</footer>
+    </StyledDiv>
+  )
+};
+
+export default Footer;
+
+const StyledDiv = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
---
+++ src/pages/fairSignUp/fairLandingPage.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Button from '../../components/landingpage/button';
+import testImg from '../../components/landingpage/assets/00.webp';
+import kpmg from '../../components/landingpage/assets/kpmg.png';
+import gtb from '../../components/landingpage/assets/gtb.jpg';
+import nlng from '../../components/landingpage/assets/nlng.png';
+import shell from '../../components/landingpage/assets/shell.png';
+import total from '../../components/landingpage/assets/total.png';
+
+const fairLandingPage = () => {
+  return (
+    <StyledDiv>
+      <section className="container-fluid">
+
+        {/* First Row */}
+        <div className="row pt-5">
+          {/* Left column */}
+          <div className="col-lg-8 m-auto banner-image-box">
+            <img src={testImg} alt="banner" className="banner-image" />
+          </div>
+
+          {/* Right column */}
+          <div className="col-lg-3 m-auto">
+            <h2 className="banner-header">Welcome to Cooplag 2020.</h2>
+            <p>Exploring Digitalization in Oil and Gas</p>
+            <div className="row">
+              <div className="col-lg-4">
+                <Button label="Sign Up" className="button" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Second Row */}
+        <div className="row">
+          <div className="col-12 partners mt-5">
+            <h2 className="partners-caption">Sponsors & Partners</h2>
+            <div className="partner-logos">
+              <div className="p-logo"><img src={kpmg} alt="logo" /></div>
+              <div className="p-logo"><img src={gtb} alt="logo" /></div>
+              <div className="p-logo"><img src={nlng} alt="logo" /></div>
+              <div className="p-logo"><img src={shell} alt="logo" /></div>
+              <div className="p-logo"><img src={total} alt="logo" /></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </StyledDiv>
+  );
+};
+
+export default fairLandingPage;
+
+const StyledDiv = styled.div`
+    Height: 100vh;
+    img {
+      width: 100%;
+    }
+    .partner-logos {
+      display: flex;
+    }
+    .p-logo {
+      width: 100px;
+      padding-right: 10px;
+    }
+    .banner-image-box {
+      width: 500px;
+    }
+
+    .banner-image {
+      height: 500px;
+    }
+
+    .banner-header {
+      color: var(--primary-color);
+    }
+    h2 {
+      font-weight: 700;
+    }
+    .partners-caption {
+      font-size: 1.2rem;
+      color: var(--dark-grey);
+    }
+`;
