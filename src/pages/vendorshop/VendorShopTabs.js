52a00a1df6fe34b1b2654dfe7e647d241391a65b
Divine Philip <dpkreativ@gmail.com>
2020-11-11 14:03:24 +0100

    properly arranged the code in VendorShopTabs.js so it's easier to look at

--- src/pages/vendorshop/VendorShopTabs.js
+++ src/pages/vendorshop/VendorShopTabs.js
@@ -1,3 +1,5 @@
+// ================================================================================
+// Imports
 import React from 'react';
 import styled from 'styled-components';
 import AdCarousels from '../../components/vendorshop/adcarousels/AdCarousels';
@@ -5,70 +7,23 @@ import ProductCard from '../../components/vendorshop/productcards/ProductCard';
 import Visitors from '../../components/vendorshop/visitors/Visitors';
 import { arrayImg } from '../../utils/data';
 
-const TabWrapper = styled.div.attrs({
-  className: 'panel panel-default bg-white mx-auto'
-})` 
-    border-radius:${(props) => props.theme.styles.borderRadius};
-    /* height:100vh; */
-    margin-top:3rem;
-    margin-bottom:3rem;
-`;
-
-const Divider = styled.hr`
-  background:${(props) => props.theme.colors.gray};
-  border-radius:${(props) => props.theme.styles.borderRadius};
-  height:.2rem;
-  margin:.7rem 2rem  ;
-`;
-const List = styled.ul.attrs({
-  // className: 'nav nav-pills',
-
-})` 
- justify-content:center;
-  color:${(props) => props.theme.colors.gray};
-  display:flex;
-  align-content:center;
-  border-radius:3rem;
-  width:70%;
-  margin:auto;
-  border:3px solid ${(props) => props.theme.colors.gray};
-  .active{
-    background:${(props) => props.theme.colors.gray};
-    padding:0rem 0;
-    color:${(props) => props.theme.colors.whiteColor};
-    border-radius:3rem;
-    border:none;
-
-    a{
-      color:${(props) => props.theme.colors.whiteColor};
-      text-decoration:none;
-      margin:1rem 1.3rem;
-      border:1px solid ${(props) => props.theme.colors.gray};
-    }
-  }
-   li {
-      flex-basis:50%;
-      padding:0rem .7rem;
-    }
-`;
-
+// ================================================================================
+// JSX Function
 const VendorShopTabs = () => {
   return (
     <TabWrapper>
-      <h4 className="text-center my-2 py-2">
-        <strong>Vendor&rsquo;s</strong>
-        {' '}
-        Shop
+      <h4 className="text-center py-3">
+        <strong>Vendor's</strong> Shop
       </h4>
       <div className="panel-heading">
         <div className="panel-title">
-          <List className="nav nav-tabs my-2">
-            <li className="active">
-              <a href="#1" data-toggle="tab">Visitors</a>
-            </li>
-            <li>
-              <a href="#2" data-toggle="tab">Shop </a>
-            </li>
+          <List className="nav nav-tabs">
+            <a href="#1" data-toggle="tab" className="active">
+              <li>Visitors</li>
+            </a>
+            <a href="#2" data-toggle="tab">
+              <li>Shop</li>
+            </a>
           </List>
         </div>
       </div>
@@ -97,3 +52,52 @@ const VendorShopTabs = () => {
 };
 
 export default VendorShopTabs;
+
+// ================================================================================
+// Styled components
+const TabWrapper = styled.div.attrs({
+  className: 'panel panel-default bg-white mx-auto',
+})`
+  border-radius: ${(props) => props.theme.styles.borderRadius};
+  height: 100%;
+  min-height: 400px;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+`;
+
+const Divider = styled.hr`
+  background: ${(props) => props.theme.colors.gray};
+  border-radius: ${(props) => props.theme.styles.borderRadius};
+  height: 0.2rem;
+  margin: 0.7rem 2rem;
+`;
+const List = styled.ul.attrs({
+  // className: 'nav nav-pills',
+})`
+  justify-content: center;
+  color: ${(props) => props.theme.colors.gray};
+  display: flex;
+  align-content: center;
+  border-radius: 3rem;
+  width: 70%;
+  margin: auto;
+  border: 3px solid ${(props) => props.theme.colors.gray};
+  .active {
+    background: ${(props) => props.theme.colors.gray};
+    padding: 0rem 0;
+    color: ${(props) => props.theme.colors.whiteColor};
+    border-radius: 3rem;
+    border: none;
+
+    a {
+      color: ${(props) => props.theme.colors.whiteColor};
+      text-decoration: none;
+      margin: 1rem 1.3rem;
+      border: 1px solid ${(props) => props.theme.colors.gray};
+    }
+  }
+  li {
+    flex-basis: 50%;
+    padding: 0rem 0.7rem;
+  }
+`;
