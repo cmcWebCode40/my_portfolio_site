621b0fa3edfb5eb601e6518a8f308421ada500db
Divine Philip <dpkreativ@gmail.com>
2020-11-12 11:11:28 +0100

    updated the UI for all fairs

--- src/pages/fairSignUp/AllFairsPage.js
+++ src/pages/fairSignUp/AllFairsPage.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { getUserToken } from '../../utils/functions/userAuth';
 import { coopLagApi } from '../../services/services';
+import Card from 'react-bootstrap/Card';
 
 export default function AllFairs(props) {
   const [fairs, setFairs] = useState([]);
@@ -33,12 +34,13 @@ export default function AllFairs(props) {
 
   return (
     <StyledDiv>
-      <h1 id="title">Explore All Fairs</h1> <hr></hr>
-      <div className="fairs-div">
-        {fairs.map((fair) => (
-          <div key={fair._id} className="per-fair">
-            <Link to={`/fair-landing-page/${fair._id}`}>
-              <div className="card" id="card-div">
+      <h2 id="title">Explore All Fairs</h2> <hr />
+      <div className="container">
+        <div className="fairs-div row mx-auto">
+          {fairs.map((fair) => (
+            <div key={fair._id} className="per-fair col-lg-4">
+              <Link to={`/fair-landing-page/${fair._id}`}>
+                {/* <div className="card" id="card-div">
                 <img src={fair.fair_banner} className="card-img-top" alt="Fair Banner" />
                 <div className="card-body">
                   <h5 className="card-title">{fair.name}</h5>
@@ -47,47 +49,48 @@ export default function AllFairs(props) {
                     <i>{fair.about_event}</i>
                   </p>
                 </div>
-              </div>
-            </Link>
-          </div>
-        ))}
+              </div> */}
+                <Card style={{ width: '18rem' }} className="mx-auto my-4 shadow">
+                  <Card.Img
+                    variant="top"
+                    src={fair.fair_banner}
+                    className="card-img-top"
+                    alt="Fair Banner"
+                  />
+                  <Card.Body>
+                    <Card.Title>{fair.name}</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of the
+                      card's content. <i>{fair.about_event}</i>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </StyledDiv>
   );
 }
 
 const StyledDiv = styled.div`
-  text-align: center;
-  margin-top: 0;
-  padding-top: 5%;
-  padding-bottom: 2%;
-  background: var(--primary-color);
-
-  @media only screen and (max-width: 800px) {
-    padding-top: 10%;
-  }
-  @media only screen and (max-width: 550px) {
-    padding-top: 12%;
-    h1 {
-      font-size: 35px;
-    }
-  }
-  @media only screen and (max-width: 440px) {
-    padding-top: 15%;
-    h1 {
-      font-size: 30px;
-    }
-  }
+  // text-align: center;
+  // margin-top: 0;
+  background-color: #f6faf5;
 
   #title {
-    color: white;
-    margin-botton: 3%;
+    color: var(--primary-color);
+    text-align: center;
+    padding: 20px;
+    font-weight: 700;
   }
+
   hr {
-    background: white;
+    // background: white;
     max-width: 800px;
     width: 80%;
-    margin-bottom: 4%;
+    margin-bottom: 20px;
   }
 
   // .fairs-div {
@@ -96,20 +99,24 @@ const StyledDiv = styled.div`
   //   flex-wrap: wrap;
   // }
 
-  .per-fair {
-    // border: 2px solid grey;
-    margin-top: 10px;
-    margin-bottom: 5px;
-    curcor: pointer;
-  }
+  // .per-fair {
+  //   // border: 2px solid grey;
+  //   margin-top: 10px;
+  //   margin-bottom: 5px;
+  //   cursor: pointer;
+  // }
 
-  #card-div {
-    width: 18rem;
-    // border: 1px dashed grey;
-  }
-  #card-div:hover {
-    opacity: 0.8;
-    transition: 0.3s;
-    box-shadow: 2px 5px 5px 5px white;
+  // #card-div {
+  //   width: 18rem;
+  //   // border: 1px dashed grey;
+  // }
+  // #card-div:hover {
+  //   opacity: 0.8;
+  //   transition: 0.3s;
+  //   box-shadow: 2px 5px 5px 5px white;
+  // }
+
+  .card-img-top {
+    height: 200px;
   }
 `;
