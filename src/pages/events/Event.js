bdb2ba396cf8b87cb68ecc93b5e846ecf785e674
Divine Philip <dpkreativ@gmail.com>
2020-11-12 15:44:50 +0100

    linting and additions

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import imageLogo from '../../assets/images/eventlanding.PNG';
+import ToggleSwitch from '../../components/toggleSwitch/ToggleSwitch';
 import EventTabMobile from './EventTabMobile';
 import EventTabs from './EventTabs';
 
 const EventWrapper = styled.div`
-  #mobile{
-    display:none;
+  #mobile {
+    display: none;
   }
   @media ${(props) => props.theme.mediaQueries.medium} {
-    #mobile{
-    display:block
-  };
-    #desktop{
-    display:none;
-  }}
-  ;
+    #mobile {
+      display: block;
+    }
+    #desktop {
+      display: none;
+    }
+  } ;
 `;
 
 const Event = () => {
+  const [selected, setSelected] = useState(false);
   return (
     <EventWrapper>
       <div id="mobile">
@@ -26,15 +28,28 @@ const Event = () => {
       </div>
       <div id="desktop" className="row m-4">
         <div className="col-8 ml-5">
-          <button type="button" className="btn btn-outline-dark m-2">Conference</button>
-          <button type="button" className="btn btn-outline-dark m-2 ">Panel session</button>
-          <button type="button" className="btn btn-outline-dark m-2 ">Networking</button>
+          <button type="button" className="btn btn-outline-dark m-2">
+            Conference
+          </button>
+          <button type="button" className="btn btn-outline-dark m-2 ">
+            Panel session
+          </button>
+          <button type="button" className="btn btn-outline-dark m-2 ">
+            Networking
+          </button>
+
           <div>
             <img src={imageLogo} alt="Landing Page" />
           </div>
         </div>
         <div className="col mx-3">
           <EventTabs />
+          <ToggleSwitch
+            selected={selected}
+            toggleSelected={() => {
+              setSelected(!selected);
+            }}
+          />
         </div>
       </div>
     </EventWrapper>
