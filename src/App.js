fc35a96ac211bfe34d5139f6b80b418fa2d344bb
unknown <infinity_michael@yahoo.com>
2020-11-18 11:48:45 +0100

    .

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import UserAuthProvider from './context/authContext';
 import store from './redux/store';
 import Routes from './routes/Routes';
 import theme from './theme/theme';
-import './libs/imageUploader';
-import 'react-toastify/dist/ReactToastify.css';
+import './icons/fontawesome';
+import './sass/main.scss';
+import './libs';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      sdjcks'sdskjd
       <UserAuthProvider>
         <Provider store={store}>
           <Routes />
-          <ToastContainer
-            hideProgressBar
-            autoClose
-          />
+
+          <ToastContainer hideProgressBar autoClose />
         </Provider>
       </UserAuthProvider>
     </ThemeProvider>
