c17a34d50a68f114fae1f93f6ccfc78dafb66649
cmcWebCode <infinity_michael@yahoo.com>
2021-04-19 00:24:04 +0100

    chore:minimal update

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,53 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { myWorks } from './data';
 
 const ProjectDetails = ({ match }) => {
 	const [project, setProject] = useState('');
-	const [items] = useState([
-		{
-			id: 1,
-			title: 'La More Collections Website',
-			description: `An E-commerce Progressive Web Application(PWA) built for selling shoes and bags. I built this web app using MongoDB,Express,Reactjs,Node(MERN)`,
-			img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1590182980/onlineshop_k7vpng.png',
-			source: 'https://github.com/cmcWebCode40/online_store_website',
-			website: 'https://lamorecollectionstore.web.app/'
-		},
-		{
-			id: 2,
-			title: 'Editorial Community Service Website',
-			description: `A Web Application  basically built for updating our host communities about all events happening around them organized by corps member (A service group). I fully developed the UI (Frontend) and also consumed the different api (REST) endpoints developed by the backend engineer
-        This app was built with HTML,CSS/SASS & REACTjs`,
-			img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1587170307/soft3_xaapbw.png',
-			source: 'https://github.com/cmcWebCode40/Editorial_fullStack',
-			website: 'http://editorialcdsgroup.live/'
-		},
-		{
-			id: 3,
-			title: 'Soft Soundz Music Zone',
-			description: `A Web App built for music  downloads and updates on current celebrity news world wide developer built with HTML5,CSS3/SASS REACTjs,TYPEDSCRIPT & GRAPHQL NB:"the admin dashboard is still in progress"`,
-			img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1587170296/soft1_jiv0id.png',
-			source: 'https://github.com/cmcWebCode40/musical_crew_website/',
-			website: 'https://soft-soundz.netlify.app/'
-		},
-		{
-			id: 4,
-			title: 'My Portfolio Website',
-			description:
-				'A Web App just to display all my works and coming up blog posts built with Reactjs ',
-			img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1587170282/soft4_su7dkv.png',
-			source: 'https://github.com/cmcWebCode40/my_portfolio_site',
-			website: 'https://chinweikemichael.tech'
-		},
-		{
-			id: 5,
-			title: 'Developers Quiz App ',
-			description: `A Progressive Web App (PWA) Built for that a user take a quick HTML quiz just for fun. This app was an internship task from (investa Ng) , built with HTML5, CSS3, JAVASCRIPT and workbox for the PWA `,
-			img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1587170269/soft2_k9saon.png',
-			source: 'https://github.com/cmcWebCode40/simple_todo_list',
-			website: 'https://cmcwebcode40.github.io/Quiz_app_investa_task/'
-		}
-	]);
+	const [items] = useState(myWorks);
 
 	const getProject = (id, data) => {
 		data.filter(state => (state.title === id ? setProject(state) : null));
---
+++ src/components/projects/data.js
@@ -0,0 +1,63 @@
+export const myWorks = [
+  {
+    id: Math.random(),
+    title: 'FoodCrowdy (No 1 Nigeria Food Store).',
+    description: `
+    FoodCrowdy is guided by the passion for innovation, providing real solutions to real problems, customer satisfaction, commitment to operational excellence and long-term thinking.
+     Frontend Stack: React.js, Material-UI, Redux-toolkit`,
+    img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1618781495/food_port_ojtlai.jpg',
+    source: 'https://github.com/cmcWebCode40',
+    website: 'https://foodcrowdy.com/'
+  },
+  {
+    id: Math.random(),
+    title: ' Global Courier Logistics',
+    description: `Global Courier Logistics has forged long term (wholesale) relationships / partnerships with all the major airlines, express courier integrators, worldwide freighter networks,
+    Frontend-Stack: React.js, Bootstrap, sass. `,
+    img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1618781502/global_port_prsg0e.jpg',
+    source: 'https://github.com/cmcWebCode40',
+    website: 'https://globalcourierlogistics.online/'
+  },
+  {
+    id: Math.random(),
+    title: 'La More Collections Website',
+    description: `An E- commerce Progressive Web Application(PWA) built for selling shoes and bags.I built this web app using MongoDB, Express, Reactjs, Node(MERN)`,
+    img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1590182980/onlineshop_k7vpng.png',
+    source: 'https://github.com/cmcWebCode40/online_store_website',
+    website: 'https://lamorecollectionstore.web.app/'
+  },
+  {
+    id: Math.random(),
+    title: 'Editorial Community Service Website',
+    description: `A Web Application  basically built for updating our host communities about all events happening around them organized by corps member(A service group).I fully developed the UI(Frontend) and also consumed the different api(REST) endpoints developed by the backend engineer
+This app was built with HTML, CSS / SASS & REACTjs`,
+    img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1587170307/soft3_xaapbw.png',
+    source: 'https://github.com/cmcWebCode40/Editorial_fullStack',
+    website: 'https://editorialcds.herokuapp.com/'
+  },
+  {
+    id: Math.random(),
+    title: 'Soft Soundz Music Zone',
+    description: `A Web App built for music  downloads and updates on current celebrity news world wide developer built with HTML5, CSS3 / SASS REACTjs, TYPEDSCRIPT & GRAPHQL NB: "the admin dashboard is still in progress"`,
+    img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1587170296/soft1_jiv0id.png',
+    source: 'https://github.com/cmcWebCode40/musical_crew_website/',
+    website: 'https://soft-soundz.netlify.app/'
+  },
+  {
+    id: Math.random(),
+    title: 'My Portfolio Website',
+    description:
+      'A Web App just to display all my works and coming up blog posts built with Reactjs ',
+    img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1587170282/soft4_su7dkv.png',
+    source: 'https://github.com/cmcWebCode40/my_portfolio_site',
+    website: 'https://chinweikemichael.netlify.app/'
+  },
+  {
+    id: Math.random(),
+    title: 'La More Collections Website',
+    description: `An E - commerce Progressive Web Application(PWA) built for selling shoes and bags.I built this web app using MongoDB, Express, Reactjs, Node(MERN)`,
+    img: 'https://res.cloudinary.com/cmcwebcode/image/upload/v1590182980/onlineshop_k7vpng.png',
+    source: 'https://github.com/cmcWebCode40/online_store_website',
+    website: 'https://lamorecollectionstore.web.app/'
+  },
+]
